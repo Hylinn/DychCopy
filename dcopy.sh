0cd078e277c8b31deef25a3840bdfeb860991598
Hylinn Taggart <htaggart@box.com>
2017-02-22 15:53:41 -0800

    Removed unused code

--- dcopy.sh
+++ dcopy.sh
@@ -3,14 +3,4 @@
 header=${1}
 destination=${2}
 
-function findFiles {
-	echo find . -type f -name "${header}*"
-}
-
-function copyFiles {
-	local fileToCopy=${1}
-	echo rsync
-}
-
-#findFiles | xargs copyFiles
 rsync -v ${header}* ${destination}
